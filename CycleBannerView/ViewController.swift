940bb12366104c3fc159cb40df868f7e47a0c051
剑明 <393012983@qq.com>
2019-06-19 14:38:25 +0800

    Initial Commit

---
+++ CycleBannerView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CycleBannerView
+//
+//  Created by JackYe on 2019/6/19.
+//  Copyright © 2019 JackYe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
